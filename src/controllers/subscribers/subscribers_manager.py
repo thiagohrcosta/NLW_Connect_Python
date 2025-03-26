6e135ba5d5731c58a697900f185d4c37d6299ef9
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-03-26 16:30:08 -0300

    Complete controllers - feature ranking

---
+++ src/controllers/subscribers/subscribers_manager.py
@@ -0,0 +1,63 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+from src.model.repositories.interfaces.subscribers_repository import SubscribersRepositoryInterface
+
+class SubscriberManager:
+  def __init__(self, subscribers_repo: SubscribersRepositoryInterface):
+    self.__subscribers_repo = subscribers_repo
+
+  def get_subscribers_link(self, http_request: HttpRequest) -> HttpResponse:
+    link = http_request.param['link']
+    event_id = http_request.params['event_id']
+
+    subs = self.__subscribers_repo.select_subscribers_by_link(link, event_id)
+
+    return self.__format_subs_by_link(subs)
+
+  def get_event_ranking(self, http_request: HttpRequest) -> HttpResponse:
+    event_id = http_request.params['event_id']
+    event_ranking = self.__subscribers_repo.get_ranking(event_id)
+
+    return self.__format_event_ranking(event_ranking)
+
+  def __format_subs_by_link(self, subs: list) -> HttpResponse:
+    formatted_subscriber = []
+
+    for sub in subs:
+      formatted_subscriber.append(
+        {
+          'name': sub.name,
+          'email': sub.email
+        }
+      )
+
+    return HttpResponse(
+      body={
+        'data': {
+          'Type': 'Subscriber',
+          'count': len(formatted_subscriber),
+          'subscribers': formatted_subscriber
+        }
+      }
+    )
+
+  def __format_event_ranking(self, event_ranking: list) -> HttpResponse:
+    formatted_event_ranking = []
+
+    for position in event_ranking:
+      formatted_event_ranking.append(
+        {
+          'link': position.link,
+          'total_subscribers': position.total
+        }
+      )
+
+    return HttpResponse(
+      body={
+        'data': {
+          'Type': 'Ranking',
+          'count': len(formatted_event_ranking),
+          'subscribers': formatted_event_ranking
+        }
+      }
+    )
